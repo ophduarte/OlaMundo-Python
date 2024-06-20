c10de25caf14a784d6f6a6a9475012211689968a
Pedro Henrique Duarte <pedro_phd@hotmail.com>
2024-06-20 16:26:47 -0300

    Exercícios em Python - Curso em Vídeo

---
+++ python_scripts/Guanabara_mundo2/desafio64_guanabara_mundo2.py
@@ -0,0 +1,3 @@
+#Crie um programa que leia vários números inteiros. O programa funcionará até que o número 999 seja digitado.
+#No final, mostre quantos números foram digitados e qual é a soma entre eles.
+
