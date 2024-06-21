fa7e84a1c117c24849c325f479ef44d0652987e8
Pedro Henrique Duarte <pedro_phd@hotmail.com>
2024-06-21 10:22:23 -0300

    Exercícios em Python - Curso em Vídeo

--- python_scripts/Guanabara_mundo2/desafio67_guanabara_mundo2.py
+++ python_scripts/Guanabara_mundo2/desafio67_guanabara_mundo2.py
@@ -1,9 +1,41 @@
 #Faça um programa que mostre a tabuada de vários números que o usuário digitar.
 #O programa será interrompido quando o usuário inserir um número negativo.
+num=""
+table=[]
+choice=""
+i=0
 
-num=input("Digite um numero")
+while True:
 
-if num.isdecimal():
-        print("é num")
-else:
-        print("não é num")
+    while not num.isdecimal():
+        num=str(input("Digite um numero inteiro para construir a tabuada:"))
+        
+        if not num.isdecimal():
+              print("O número digitado não é inteiro.")
+    
+    table.append(int(num))    
+
+    while choice!="N" and choice!="S":
+        choice=str(input("\nDeseja digitar outro número? [S/N]\nEscolha:")).upper()
+        
+        if choice!="N" and choice!="S":
+             print("Entrada inválida.\n")
+    
+    if choice=="S":
+        num=""
+        choice=""
+
+    if choice=="N":
+         break
+
+while i<len(table):
+    j=0
+    
+    print(f"\nTabuada do {table[i]}")
+    
+    for j in range (0,11,1):
+        print(f"{table[i]}x{j} = {table[i]*j}")
+    
+    print("---"*10)
+    
+    i += 1
